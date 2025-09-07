f2dcb6e028d1959e07fce09c1d9260f314aa6061
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2025-09-07 16:54:28 -0700

    i2c comparison code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUComparatorTest.kt
@@ -0,0 +1,99 @@
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver
+import com.qualcomm.hardware.kauailabs.NavxMicroNavigationSensor
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.HardwareMap
+import com.qualcomm.robotcore.hardware.IMU
+import io.github.gearup12499.taskshark.FastScheduler
+import io.github.gearup12499.taskshark.Task
+import io.github.gearup12499.taskshark.api.BuiltInTags
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference
+import org.firstinspires.ftc.teamcode.hardware.HardwareMapper
+import org.firstinspires.ftc.teamcode.hardware.HardwareName
+
+
+@TeleOp
+class IMUComparatorTest : LinearOpMode() {
+    class Hw(map: HardwareMap) : HardwareMapper(map) {
+        @HardwareName("navx")
+        lateinit var navX: NavxMicroNavigationSensor
+
+        @HardwareName("PinPoint")
+        lateinit var pinpoint: GoBildaPinpointDriver
+
+        @HardwareName("imu")
+        lateinit var builtInIMU: IMU
+
+        init {
+        }
+    }
+
+    override fun runOpMode() {
+        val hw = Hw(hardwareMap)
+        val sched = FastScheduler()
+        val imuOptions: IMU.Parameters = IMU.Parameters(
+            RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.LEFT
+            )
+        )
+        val setup = sched.add(object : Task.Anonymous() {
+            override fun onStart() {
+                hw.builtInIMU.initialize(imuOptions)
+                hw.builtInIMU.resetYaw()
+                hw.navX.initialize()
+                hw.pinpoint.resetPosAndIMU()
+            }
+
+            override fun onTick(): Boolean {
+                telemetry.addLine("Waiting for calibration to finish.")
+                val navXCalib = hw.navX.isCalibrating
+                telemetry.addData("NavX", if (navXCalib) "..." else "done")
+                val pinpointCalib =
+                    hw.pinpoint.deviceStatus == GoBildaPinpointDriver.DeviceStatus.CALIBRATING
+                telemetry.addData("Pinpoint", if (pinpointCalib) "..." else "done")
+                telemetry.addData("Pinpoint status", hw.pinpoint.deviceStatus)
+                telemetry.update()
+                return !(navXCalib || pinpointCalib)
+            }
+        })
+        setup.then(object : Task.Anonymous() {
+            private val tags = setOf(BuiltInTags.DAEMON)
+            override fun getTags(): Set<String> = tags
+
+            override fun onTick(): Boolean {
+                hw.pinpoint.update()
+                return false
+            }
+        })
+        setup.then(object : Task.Anonymous() {
+            override fun onTick(): Boolean {
+                val navX = hw.navX.getAngularOrientation(
+                    AxesReference.INTRINSIC,
+                    AxesOrder.ZYX,
+                    AngleUnit.DEGREES
+                ).firstAngle
+                val pinpoint = hw.pinpoint.getHeading(AngleUnit.DEGREES)
+                val internal = hw.builtInIMU.getRobotOrientation(
+                    AxesReference.INTRINSIC,
+                    AxesOrder.ZYX,
+                    AngleUnit.DEGREES
+                ).firstAngle
+                telemetry.addData("NavXIMU", "%.1fdeg".format(navX))
+                telemetry.addData("Pinpoint", "%.1fdeg".format(pinpoint))
+                telemetry.addData("Builtin", "%.1fdeg".format(internal))
+                telemetry.update()
+                return false
+            }
+        })
+
+        waitForStart()
+        while (opModeIsActive()) sched.tick()
+    }
+
+}
