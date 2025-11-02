1c3588ef02429c9a60dec4a96538b1880865ffd0
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2025-11-02 10:30:33 -0800

    Reformat code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/CompBotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/CompBotHardware.java
@@ -3,19 +3,21 @@ package org.firstinspires.ftc.teamcode.hardware;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class CompBotHardware extends HardwareMapper{
+public class CompBotHardware extends HardwareMapper {
 
-    public double[] redFarStart = {-63,-16,0};
+    public double[] redFarStart = {-63, -16, 0};
 
-    public double[] shootPos = {12,-12,-Math.PI/4};
+    public double[] shootPos = {12, -12, -Math.PI / 4};
 
-    public double[] blueBase = {-38,-33,0};
+    public double[] blueBase = {-38, -33, 0};
 
-    public double[] gateWaypoint = {0,-48,0};
+    public double[] gateWaypoint = {0, -48, 0};
 
-    public double[] gatePos = {0,-55,0};
+    public double[] gatePos = {0, -55, 0};
+
     @HardwareName("pinpoint")
     public GoBildaPinpoint2Driver PinPoint;
+
     @HardwareName("frontLeft")
     @Reversed
     @ZeroPower(DcMotor.ZeroPowerBehavior.BRAKE)
