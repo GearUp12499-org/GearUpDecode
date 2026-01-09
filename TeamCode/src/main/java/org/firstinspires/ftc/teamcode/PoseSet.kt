82a83b0b7a11b66da962912d4c885640ec86a2e8
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2026-01-08 18:13:47 -0800

    auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PoseSet.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PoseSet.kt
@@ -16,12 +16,13 @@ class PoseSet private constructor(val invert: Boolean) {
         private val readAT = RobotPose(46.92, -11.92, -2.55)
         private val closeShoot = RobotPose(60.0, -12.48, 0.55 * Math.PI)
         private val midShoot = RobotPose(24.0, -24.0, 3 * Math.PI / 4)
+        private val midShoot2 = RobotPose(36.0, -12.0, 2 * Math.PI / 3)
         private val farShoot = RobotPose(-55.0, -12.39, 2.76)
-        private val set1pos = RobotPose(12.0, -30.375, -Math.PI / 2)
+        private val set1pos = RobotPose(12.0, -24.0, -Math.PI / 2)
         private val set1out = RobotPose(12.0, -54.125, -Math.PI / 2)
-        private val set2pos = RobotPose(-12.0, -30.375, -Math.PI / 2)
+        private val set2pos = RobotPose(-12.0, -24.0, -Math.PI / 2)
         private val set2out = RobotPose(-12.0, -61.625, -Math.PI / 2)
-        private val set3pos = RobotPose(-36.0, -30.375, -Math.PI / 2)
+        private val set3pos = RobotPose(-36.0, -24.0, -Math.PI / 2)
         private val set3out = RobotPose(-36.0,  -61.625, -Math.PI / 2)
         private val auto1finish = RobotPose(0.0,  -48.0, Math.PI)
         private val blueBase = RobotPose(-38.0, -33.0, 0.0)
@@ -47,6 +48,8 @@ class PoseSet private constructor(val invert: Boolean) {
     val closeShoot = Companion.closeShoot.bind
     @JvmField
     val midShoot = Companion.midShoot.bind
+    @JvmField
+    val midShoot2 = Companion.midShoot2.bind
     @JvmField
     val farShoot = Companion.farShoot.bind
     @JvmField
