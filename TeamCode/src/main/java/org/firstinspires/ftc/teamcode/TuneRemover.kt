cebb486f89e4bd442aa757c54a591903ad11c270
ryanc <ryanchen0593@gmail.com>
2026-02-17 16:39:35 -0800

    farstrafe fully functional :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TuneRemover.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TuneRemover.kt
@@ -120,17 +120,15 @@ class TuneRemover : LinearOpMode() {
 
             if (gamepad1.a && !wasA) {
                 sch2.stopUsing(Locks.DRIVE_MOTORS)
-                sch2.add(REmover.drive2Pose2(hw, REmover.RobotPose(72.0, 0.0, 0.0)))
+                sch2.add(REmover.drive2Pose2(hw, REmover.RobotPose(48.0, 0.0, 0.0)))
             }
             else if (gamepad1.b && !wasB) {
                 sch2.stopUsing(Locks.DRIVE_MOTORS)
-                sch2.add(REmover.drive2Pose2(hw, poseSet.set2pos))
-                sch2.add(REmover.drive2Pose2(hw, poseSet.set2out))
+                sch2.add(REmover.drive2Pose2(hw, REmover.RobotPose(0.0, -48.0 , 0.0)))
             }
             else if (gamepad1.x && !wasX) {
                 sch2.stopUsing(Locks.DRIVE_MOTORS)
-                sch2.add(REmover.drive2Pose2(hw, poseSet.set2pos, waypoint = true))
-                sch2.add(REmover.drive2Pose2(hw, poseSet.set2out))
+                sch2.add(REmover.drive2Pose2(hw, REmover.RobotPose(0.0,0.0,0.0)))
 
             }
             else if (gamepad1.y && !wasY) {
