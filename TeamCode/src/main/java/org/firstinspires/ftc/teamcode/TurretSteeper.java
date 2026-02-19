1a37c02bbee3ba12f6e83f2a3f656b8964d1c8c7
abirarun <146149304+abirarun@users.noreply.github.com>
2026-02-18 20:09:38 -0800

    the pid is.bad

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurretSteeper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurretSteeper.java
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @TeleOp
@@ -12,10 +15,22 @@ public class TurretSteeper extends LinearOpMode {
     CRServo servoTurret1;
     CRServo servoTurret2;
     DcMotorEx intake2;
-    double basePower = 0.3; // needs to be tested - this is the power to barely move the turret
-    double P = 0.0025;
+    double basePower = 0.14;
+    double P = 0.001;
+    double I = 0.000005;
+    double D = 0.000030;
+//    double D = 0.001;
+    double maxI = 10000.0;
+    double deadbandTicks = 136.0;
+    double minPowerErrorTicks = 320.0;
+    double iZoneTicks = 650.0;
+    double nearTargetIClamp = 2500.0;
+    double integralError = 0.0;
+    double prevError = 0.0;
+    long lastPidTime = 0L;
+    boolean resetPid = true;
 
-    private static final double TICKS_PER_DEGREE = (double) 67.9;
+    private static final double TICKS_PER_DEGREE = 67.9;
 
     private void turnTurret(double power, CRServo t1, CRServo t2){
         double appliedpower = -power;
@@ -24,39 +39,90 @@ public class TurretSteeper extends LinearOpMode {
     }
 
     private double limit(int currentPos, double power){
-        // IMPORTANT: positive position is in the direction of negative power (negative power makes the turret turn in a pos direction)
-        telemetry.addData("Current Pos: ", currentPos);
-        telemetry.addData("Current Power: ", power);
-        telemetry.update();
-        if(currentPos > posLimit && power > 0){
+        if(currentPos >= posLimit && power > 0){
             return 0.0;
         }
-        if(currentPos < negLimit && power < 0) {
+        if(currentPos <= negLimit && power < 0) {
             return 0.0;
         }
         return power;
     }
 
-    private void setTurnPosition(double deg){ // NOT WORKING YET - NEED TO FIX SIGNS
-        double targetTicks = deg * TICKS_PER_DEGREE;
-        double error = targetTicks - intake2.getCurrentPosition();
-        double output = P * error;
-        if(output > 1){
-            output = 1;
+    private void setTurnPosition(double deg){
+        double targetTicks = -deg * TICKS_PER_DEGREE;
+        if (targetTicks > posLimit) {
+            targetTicks = posLimit;
+        } else if (targetTicks < negLimit) {
+            targetTicks = negLimit;
         }
-        if(Math.abs(error) <= 136.0 && Math.abs(output) < basePower) {
-            output = 0;
+        double error = targetTicks - intake2.getCurrentPosition();
+        long now = System.nanoTime();
+        double dt = 0.0;
+        if (lastPidTime != 0L) {
+            dt = (now - lastPidTime) / 1e9;
+        }
+        lastPidTime = now;
+
+        if (Math.abs(error) <= deadbandTicks) {
+            prevError = error;
+            integralError = 0.0;
+            this.turnTurret(0.0, servoTurret1, servoTurret2);
+            return;
+        }
+
+        if (resetPid) {
+            prevError = error;
+            integralError = 0.0;
+            resetPid = false;
+        }
+
+        if (error * prevError < 0.0) {
+            integralError = 0.0;
+        }
+
+        if (Math.abs(error) <= iZoneTicks) {
+            integralError += error * dt;
+            double activeIClamp = Math.abs(error) <= minPowerErrorTicks ? nearTargetIClamp : maxI;
+            if (integralError > activeIClamp) {
+                integralError = activeIClamp;
+            } else if (integralError < -activeIClamp) {
+                integralError = -activeIClamp;
+            }
+        } else {
+            integralError *= 0.9;
+        }
+
+        double derivative = 0.0;
+        if (dt > 0.0) {
+            derivative = (error - prevError) / dt;
+        }
+        prevError = error;
+
+
+        telemetry.addData("P * error", (P * error));
+        telemetry.addData("I * error", (I * integralError));
+        telemetry.addData("D * error", (D * derivative));
+        Log.i("!", String.format("%.2f %.2f %.2f PID", P*error, I*integralError, D*derivative));
+        telemetry.update();
+        double output = (P * error) + (I * integralError) + (D * derivative);
+        if(output > 1.0){
+            output = 1.0;
+        } else if (output < -1.0) {
+            output = -1.0;
+        }
+        if (Math.abs(error) > minPowerErrorTicks && Math.abs(output) < basePower) {
+            output = Math.copySign(basePower, output);
         }
         this.turnTurret(limit(intake2.getCurrentPosition(), output), servoTurret1, servoTurret2);
-
     }
 
-    // ticks = -6155 --> 90 degrees
     @Override
     public void runOpMode() throws InterruptedException {
         servoTurret1 = hardwareMap.get(CRServo.class, "turret1");
         servoTurret2 = hardwareMap.get(CRServo.class, "turret2");
         intake2 = hardwareMap.get(DcMotorEx.class, "intake2");
+        intake2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        intake2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         negLimit = -9400.0;
         posLimit = 9400.0;
         double servoPower = 1;
@@ -64,20 +130,8 @@ public class TurretSteeper extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-
-            if(gamepad1.a){
-                this.turnTurret(limit(intake2.getCurrentPosition(), servoPower), servoTurret1, servoTurret2);
-            } else if(gamepad1.b){
-                this.turnTurret(limit(intake2.getCurrentPosition(), -servoPower), servoTurret1, servoTurret2);
-            } else if(gamepad1.x){
                 this.setTurnPosition(90.0);
             }
-            else {
-                this.turnTurret(0.0, servoTurret1, servoTurret2);
-            }
-//            telemetry.addData("Current Position (Ticks): ", intake2.getCurrentPosition());
-//            telemetry.update();
         }
 
     }
-}
