76bce062ae3b8b44067dd9433014850f21c691a2
Liam Doran <65523663+Minerbot360@users.noreply.github.com>
2026-03-05 17:44:30 -0800

    New linear interpolation for shooter speed and hood position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/CompBot2Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/CompBot2Hardware.java
@@ -276,13 +276,17 @@ public class CompBot2Hardware extends HardwareMapper {
      * @return hood, speed
      */
     public static Pair<Double, Double> hoodAndSpeed(double distance) {
-        boolean isUp = distance >= SHOOT_HOOD_UP_DIST;
-        boolean isReallyFar = distance >= SHOOT_MAX_DIST;
-        double hood = isReallyFar ? HOOD_UP : isUp ? HOOD_50 : HOOD_DOWN;
+//        boolean isUp = distance >= SHOOT_HOOD_UP_DIST;
+        isReallyFar = distance >= SHOOT_MAX_DIST;
+//        double hood = isReallyFar ? HOOD_UP : isUp ? HOOD_50 : HOOD_DOWN;
+        double hood;
         double speed;
-        if (isReallyFar) speed = SHOOT_FAR_RANGE;
-        else if (isUp) speed = 4.7215 * distance + 1119.6191; // https://www.desmos.com/calculator/ddauz4m00v
-        else speed = 8.8412 * distance + 927.2010;
+//        if (isReallyFar) speed = SHOOT_FAR_RANGE;
+//        else if (isUp) speed = 4.8215 * distance + 1140; // https://www.desmos.com/calculator/ddauz4m00v
+        speed = 6.81246 * distance + 1075.16505;
+        hood = 0.00492724 * distance + 0.0769453;
+        if (hood > 0.5578) hood = 0.5578;
+        else if (hood < 0.1817) hood = 0.1817;
         return new Pair<>(hood, speed);
     }
 }
