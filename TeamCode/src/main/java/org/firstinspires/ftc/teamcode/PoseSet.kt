9b070247d9cdc23ad7c01bc4a39f695166e9ed4b
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2026-03-05 19:34:04 -0800

    auto2 tweaks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PoseSet.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PoseSet.kt
@@ -30,7 +30,7 @@ class PoseSet private constructor(val invert: Boolean) {
         private val closeShoot3 = RobotPose(48.66, -25.0, 2.06)
         private val midShoot = RobotPose(24.0, -24.0, 2.39, -79.0)
         private val midShoot2 = RobotPose(36.0, -12.0, 2 * Math.PI / 3, -93.0)
-        private val farShoot = RobotPose(-55.0, -12.39, 2.65 )
+        private val farShoot = RobotPose(-55.0, -12.39, 2.705 )
         private val farShoot2 = RobotPose(-64.0, -29.0, -Math.PI/2,-105.0)
 
 
