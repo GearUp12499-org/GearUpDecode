4e1b71efa03744e30587be4b416ecd37d01c1512
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2026-05-29 08:26:30 -0700

    Mirror filter on blue II

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/Kalman.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/Kalman.kt
@@ -279,7 +279,7 @@ class Kalman(
 
             structuralErrorX = 0.1606325833
             structuralErrorY = 1.280544028 * reverse
-        } else if (stateY < -48 * (if (red) 1 else -1)) {
+        } else if (reverse * stateY < -48) {
             R = SimpleMatrix(
                 arrayOf<DoubleArray?>(
                     doubleArrayOf(4.0, -0.0842, 0.0),
